3d8721c105796aa762e306c7fbd11b28f9cd6ef8
Gabor Ratky <rgabo@rgabostyle.com>
2010-05-16 16:42:08 +0200

    Added additional dependencies and Tomdoc summary to Readability

---
+++ lib/readability.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'harmony'
+
+require 'readability/document'
+
+# Run the Arc90 Lab Experiment Readability on a Nokogiri document.
+# TODO: Add example
+#
+module Readability
+end
---
+++ lib/readability/document.rb
@@ -0,0 +1,4 @@
+module Readability
+  class Document
+  end
+end
